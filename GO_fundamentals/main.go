91b6c49dcc0c80ce7d9668fc0c7243a5cea4a7fe
MartoMcfly <martinpelaez@hotmail.es>
2022-12-19 22:13:09 -0500

    Feature commited

---
+++ GO_fundamentals/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, there! what are you up to so late?")
+}
+
+func main2() {
+	fmt.Println("Hi, there! what are you up to so late?")
+}
+
+//How do we run the code in our project?
+
+//What does package main mean?
+
+//What does import fmt mean
+
+//whats that func thing
+
+//How is the main.go organizaed
+
+//--------------------
+//golang.org/pkg
