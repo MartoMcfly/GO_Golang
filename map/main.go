5955f79a766b9d9ae349892f24514ce0bf00ac48
MartoMcfly <martinpelaez@hotmail.es>
2023-02-04 11:16:32 -0500

    Additional exexersice on GO maps

--- map/main.go
+++ map/main.go
@@ -6,14 +6,23 @@ func main() {
 
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	//colors := make(map[int]string)
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors["white"] = "#ffffff"
+	//colors["white"] = "#ffffff"
+
+	//colors[10] = "#ffffff"
 
 	fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
